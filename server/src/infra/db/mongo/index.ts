4a14e097a4c8d2061fd5683fbae5d3640c106df6
Dinero <dineroprojects@gmail.com>
2025-12-04 15:21:19 +0100

    `Refactor MongoDB connection logic and add close connection function`

--- server/src/infra/db/mongo/index.ts
+++ server/src/infra/db/mongo/index.ts
@@ -3,6 +3,15 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export async function createMongooseConnection() {
-  await mongoose.connect(process.env.MONGO_URL || "");
-  return mongoose;
+  const uri = process.env.MONGO_URI;
+
+  if (!uri) {
+    throw new Error("MONGO_URI is not defined in the environment variables");
+  }
+
+  return await mongoose.connect(uri);
+}
+
+export async function closeMongooseConnection() {
+  return await mongoose.connection.close();
 }
